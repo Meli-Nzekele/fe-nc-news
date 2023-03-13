ff63278fb3f4d219e3e780698df608142cf3fc46
Melissa Nzekele Courtney <melissanzekelecourtney@gmail.com>
2023-03-13 16:12:56 +0000

    add react-router and components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import Header from "./Components/Header";
+import NavBar from "./Components/NavBar";
+import Articles from "./Components/Articles";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header>
+        <Header />
+      </Header>
+      <nav>
+        <NavBar />
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<Articles />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
