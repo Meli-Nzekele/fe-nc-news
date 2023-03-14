26ab6ebd76cc75a635964eee25352f026bc491f1
Melissa Nzekele Courtney <melissanzekelecourtney@gmail.com>
2023-03-14 09:23:17 +0000

    add Link to path - /

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
   return (
     <nav>
       <ul className="nav-bar">
-        <li>All Articles</li>
+        <li>
+          {" "}
+          <Link className="Link" to="/">
+            All Articles
+          </Link>
+        </li>
         <li>Coding</li>
         <li>Football</li>
         <li>Cooking</li>
