73bebef24e996d84435c4670c4fe19b431f00c7a
Melissa Nzekele Courtney <melissanzekelecourtney@gmail.com>
2023-03-14 09:22:21 +0000

    component retrieves/displays a single article and data

--- src/Components/SingleArticle.jsx
+++ src/Components/SingleArticle.jsx
@@ -1,21 +1,53 @@
 import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 import { getArticlesById } from "../Utils/api";
 
 const SingleArticle = () => {
   const [singleArticle, setSingleArticle] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { article_id } = useParams();
 
   useEffect(() => {
-    getArticlesById().then((articlesData) => {
-      setSingleArticle(articlesData);
-    });
-  }, []);
+    setIsLoading(true);
 
-  return (
-    <ul className="articles">
-      {articles.map((article) => {
-        return <ArticleCard key={article.article_id} article={article} />;
-      })}
-    </ul>
+    getArticlesById(article_id).then((articleData) => {
+      setSingleArticle(articleData);
+      setIsLoading(false);
+    });
+  }, [article_id]);
+
+  return isLoading ? (
+    <section>
+      <h2 className="loading-header">Loading...</h2>
+    </section>
+  ) : (
+    <section className="single-article">
+      <ul>
+        <li>
+          <article>
+            <p className="single-article-topic">{singleArticle.topic}</p>
+            <h3>{singleArticle.title}</h3>
+            <p>By: {singleArticle.author}</p>
+            <p>Date: {singleArticle.created_at}</p>
+            <img
+              src={singleArticle.article_img_url}
+              alt={singleArticle.title}
+              id="single-article-img"
+            />
+            <p>{singleArticle.body}</p>
+
+            <p>Votes: {singleArticle.votes}</p>
+            <p>Comments: {singleArticle.comment_count}</p>
+            {/* Date to be formatted at a later stage */}
+            <Link to="/">
+              <button>Back to Articles</button>
+            </Link>
+            <hr />
+          </article>
+        </li>
+      </ul>
+    </section>
   );
 };
 
