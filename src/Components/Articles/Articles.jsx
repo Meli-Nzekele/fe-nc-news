5d57885559e184d73a17a2aa3c9edaf022ddfb08
Melissa Nzekele Courtney <melissanzekelecourtney@gmail.com>
2023-03-16 15:47:28 +0000

    add sort by and order by functionality to dropdown menu, and queries added to correct URL path

--- src/Components/Articles/Articles.jsx
+++ src/Components/Articles/Articles.jsx
@@ -1,21 +1,35 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { getArticles } from "../../Utils/api";
+import { useParams, useSearchParams } from "react-router-dom";
+import { getArticlesByParams } from "../../Utils/api";
 import ArticleCard from "./ArticleCard";
 
 const Articles = () => {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  const [sortBy, setSortBy] = useState("article_id");
+  const [orderBy, setOrderBy] = useState("DESC");
+  const [searchParams, setSearchParams] = useSearchParams();
   const { topic } = useParams();
 
   useEffect(() => {
     setIsLoading(true);
-    getArticles(topic).then((articlesData) => {
+    getArticlesByParams(topic, searchParams).then((articlesData) => {
       setArticles(articlesData);
       setIsLoading(false);
     });
-  }, [topic]);
+  }, [topic, searchParams]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    let params = {
+      sort_by: sortBy,
+      order_by: orderBy,
+    };
+
+    const newSearchParams = new URLSearchParams(params);
+    return setSearchParams(newSearchParams);
+  };
 
   return isLoading ? (
     <section>
@@ -23,8 +37,47 @@ const Articles = () => {
     </section>
   ) : (
     <main className="articles-main">
+      <h2 className="topic-header">{topic ? topic : "All Articles"}</h2>
+      <section className="sort-dropdown">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label className="sort-dropdown-title" htmlFor="sort-by">
+              Sort By:
+            </label>
+
+            <select
+              id="sort-by"
+              name="sort-by"
+              value={sortBy}
+              onChange={(event) => setSortBy(event.target.value)}
+            >
+              <option value="article_id">Default</option>
+              <option value="title">Article</option>
+              <option value="votes">Votes</option>
+              <option value="comment_count">Comments</option>
+              <option value="created_at">Date</option>
+            </select>
+          </div>
+          <div>
+            <label className="orderby-title" htmlFor="order-by">
+              Order:
+            </label>
+            <select
+              id="order-by"
+              name="order-by"
+              value={orderBy}
+              onChange={(event) => setOrderBy(event.target.value)}
+            >
+              <option value="DESC">desc</option>
+              <option value="ASC">asc</option>
+            </select>
+          </div>
+          <button className="queries-submit-btn" type="submit">
+            Search
+          </button>
+        </form>
+      </section>
       <section>
-        <h2 className="topic-header">{topic ? topic : "All Articles"}</h2>
         <ul className="articles">
           {articles.map((article) => {
             return <ArticleCard key={article.article_id} article={article} />;
