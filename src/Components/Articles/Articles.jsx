1181145ad7abba3bece79361b91ae723fc7e413b
Melissa Nzekele Courtney <melissanzekelecourtney@gmail.com>
2023-03-16 11:54:33 +0000

    add functionality to view articles of a specific topic

--- src/Components/Articles/Articles.jsx
+++ src/Components/Articles/Articles.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { getArticles } from "../../Utils/api";
 import ArticleCard from "./ArticleCard";
 
@@ -6,22 +7,24 @@ const Articles = () => {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  const { topic } = useParams();
+
   useEffect(() => {
     setIsLoading(true);
-
-    getArticles().then((articlesData) => {
+    getArticles(topic).then((articlesData) => {
       setArticles(articlesData);
       setIsLoading(false);
     });
-  }, []);
+  }, [topic]);
 
   return isLoading ? (
     <section>
       <h2 className="loading-header">Loading...</h2>
     </section>
   ) : (
-    <main>
+    <main className="articles-main">
       <section>
+        <h2 className="topic-header">{topic ? topic : "All Articles"}</h2>
         <ul className="articles">
           {articles.map((article) => {
             return <ArticleCard key={article.article_id} article={article} />;
