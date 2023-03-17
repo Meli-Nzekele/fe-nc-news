e2ebc3e11816858fb09f62558d35be87adce8008
Melissa Nzekele Courtney <melissanzekelecourtney@gmail.com>
2023-03-17 22:19:26 +0000

    Added UserContext, Login button/Link displays current logged in user

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../Contexts/User";
 
 const Header = () => {
+  const { loggedInUser, user } = useContext(UserContext);
+
   return (
     <header>
       <h1>NC-NEWS</h1>
       <Link to="/Login" className="Login-btn-Link">
-        <button className="Login-btn">
-          <p>Login</p> <i className="fa-solid fa-user"></i>
+        <button className={loggedInUser ? "Logged-in-btn" : "Logged-out-btn"}>
+          <p>{loggedInUser ? `${user}` : "Login"}</p>{" "}
+          <i className="fa-solid fa-user"></i>
         </button>
       </Link>
     </header>
